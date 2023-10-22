57225129394790967c7b3e0d486fdbdcdd52645b
Augusto Developer <aug.dev7@gmail.com>
2023-10-22 10:18:20 -0300

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { siteConfig } from '@/config';
+
 import Header from "@/components/Header";
 import "./css/globals.css";
 import type { Metadata } from "next";
@@ -7,9 +9,35 @@ import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: {
-    template: "shopping_mart",
-    default: "Shopping Mart - A place for all!",
+    template: '%s | AGÊNCIA UP.EXPERT',
+    default: 'Agência full service'
   },
+  description: 'MARKETIGN DIGITAL EM BRASILIA | (61) 9 8669-2775',
+  verification: {
+    google: 'google-site-verification=123123123'
+  },
+  manifest: '/manifest.json',
+  authors: [{ name: 'AGÊNCIA UP.EXPERT' }],
+  metadataBase: new URL(`${siteConfig.url}`),
+  openGraph: {
+    type: 'website',
+    url: `${siteConfig.url}/cover.jpg`,
+    title: siteConfig.title,
+    description: siteConfig.description,
+    siteName: 'UP.EXPERT',
+    images: [
+      {
+        url: `${siteConfig.url}/cover.jpg`
+      }
+    ]
+  },
+  robots: 'index, follow',
+  twitter: {
+    card: 'summary_large_image',
+    title: siteConfig.title,
+    description: siteConfig.description,
+    images: [`${siteConfig.url}/cover.jpg`]
+  }
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
-import Banner from "@/components/Banner";
-import Products from "@/components/Products";
+'use client'
+ 
+import { useState } from 'react'
+import dynamic from 'next/dynamic'
+
+const Banner = dynamic(() => import('@/components/Banner'))
+const Products = dynamic(() => import('@/components/Products'), { ssr: false })
 
 export default function Home() {
   return (
